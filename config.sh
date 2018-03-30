d9821f7ca70792cb93389f1219a6745653113220
WaveGenerator <36247124+WaveGenerator@users.noreply.github.com>
2018-03-30 16:59:01 +0300

    Add files via upload

--- config.sh
+++ config.sh
@@ -10,9 +10,10 @@
 #
 # 1. Place your files into system folder (delete the placeholder file)
 # 2. Fill in your module's info into module.prop
-# 3. Configure the settings in this file (config.sh)
-# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
-# 5. Add your additional or modified system properties into common/system.prop
+# 3. Configure the settings in this file (common/config.sh)
+# 4. For advanced features, add shell commands into the script files under common:
+#    post-fs-data.sh, service.sh
+# 5. For changing props, add your additional/modified props into common/system.prop
 #
 ##########################################################################################
 
@@ -41,7 +42,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "      Mi5s camera lib hacks    "
   ui_print "*******************************"
 }
 
@@ -50,8 +51,10 @@ print_modname() {
 ##########################################################################################
 
 # List all directories you want to directly replace in the system
-# Check the documentations for more info about how Magic Mount works, and why you need this
+# By default Magisk will merge your files with the original system
+# Directories listed here however, will be directly mounted to the correspond directory in the system
 
+# You don't need to remove the example below, these values will be overwritten by your own list
 # This is an example
 REPLACE="
 /system/app/Youtube
@@ -60,7 +63,7 @@ REPLACE="
 /system/framework
 "
 
-# Construct your own list here, it will override the example above
+# Construct your own list here, it will overwrite the example
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
 "
@@ -82,18 +85,32 @@ set_permissions() {
   # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
   # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
   # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_liteon_hfr_60_3a.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_liteon_hfr_120_3a.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_liteon_hfr_240_3a.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_liteon_video_16M_3a.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_liteon_zsl_preview.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_liteon_zsl_video.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libmmcamera2_mct.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libmmcamera_imx378_liteon.so 0 0 0644
+  set_perm $MODPATH/system/etc/media_profiles.xml 0 0 0644
+  set_perm $MODPATH/system/etc/media_codecs.xml 0 0 0644  
+  set_perm $MODPATH/system/etc/media_codecs_google_video.xml 0 0 0644  
+  set_perm $MODPATH/system/vendor/lib/libmmcamera_imx378_semco.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_semco_hfr_60_3a.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_semco_hfr_120_3a.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_semco_hfr_240_3a.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_semco_video_16M_3a.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_semco_zsl_preview.so 0 0 0644
+  set_perm $MODPATH/system/vendor/lib/libchromatix_imx378_semco_zsl_video.so 0 0 0644
+  set_perm $MODPATH/system/lib/hw/camera.msm8996.so 0 0 0644
+  set_perm $MODPATH/system/etc/thermal-engine-8996.conf 0 0 0644
+  set_perm $MODPATH/system/vendor/etc/media_profiles_V1_0.xml 0 0 0644
+  set_perm $MODPATH/system/vendor/etc/thermal-engine-8996.conf 0 0 0644
+  set_perm $MODPATH/system/vendor/etc/media_codecs.xml 0 0 0644
+  set_perm $MODPATH/system/vendor/etc/mixer_paths_tasha.xml 0 0 0644
+  set_perm $MODPATH/system/vendor/etc/mixer_paths.xml 0 0 0644
 
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
 }
-
-##########################################################################################
-# Custom Functions
-##########################################################################################
-
-# This file (config.sh) will be sourced by the main flash script after util_functions.sh
-# If you need custom logic, please add them here as functions, and call these functions in
-# update-binary. Refrain from adding code directly into update-binary, as it will make it
-# difficult for you to migrate your modules to newer template versions.
-# Make update-binary as clean as possible, try to only do function calls in it.
-
